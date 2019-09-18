79945e97b6e83055d04e64b7f296494a7059aada
Ryder <36494378+Spork1023@users.noreply.github.com>
2019-09-17 17:06:11 -0700

    Add files via upload
    finished module 1! onto module 2

---
+++ _00_StringManipulation.java
@@ -0,0 +1,23 @@
+package _02_String_Manipulation;
+
+public class _00_StringManipulation {
+	// Complete the exercises at the following links
+	// http://codingbat.com/java/String-1
+	// http://codingbat.com/java/String-2
+	// http://codingbat.com/java/String-3
+//	//public boolean xyzMiddle(String str) {
+//	  int cRight = str.length() - (str.indexOf("xyz")+4);
+//	  boolean rt = false;
+//	  if(cRight == str.indexOf("xyz") - 1 || cRight+1 == str.indexOf("xyz") - 1 
+//	  || cRight == str.indexOf("xyz")){
+//	    if(str.indexOf("xyz") != -1){
+//	    rt = true;
+//	    }
+//	    }
+//	    else {
+//	      rt = false;
+//	    }
+//	    return rt;
+//	  }
+
+}
